acb90e8ef64d9e03bdd869086906e78d0009e6a5
darwinum <dun34@hotmail.com>
2019-09-26 15:08:18 -0500

    se agregan las propiedades para el contexto.

--- Natillera.DataAccess/NatilleraDBContext.cs
+++ Natillera.DataAccess/NatilleraDBContext.cs
@@ -1,16 +1,31 @@
-﻿
-
-
-namespace Natillera.DataAccess
+﻿namespace Natillera.DataAccess
 {
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
+    using Natillera.DataAccessContract.Entidades;
 
-   public class NatilleraDBContext : IdentityDbContext<ApplicationUser>
+    public class NatilleraDBContext : IdentityDbContext<ApplicationUser>
     {
-        //public DbSet<Blog> Blogs { get; set; }
-        //public DbSet<Post> Posts { get; set; }
-       
+        //cuando se hereda de IdentityDbContext<ApplicationUser>, se crean las tablas para 
+        //el manejo de usuarios.
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AplicationDbContext"/> class.
+        /// </summary>
+        /// <param name="opcion">The opcion<see cref="DbContextOptions{AplicationDbContext}"/></param>
+        public NatilleraDBContext(DbContextOptions<NatilleraDBContext> opcion) : base(opcion)
+        {
+        }
 
+        public DbSet<NatilleraEntity> Natilleras { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            ///controlar la concurrencia, se valida esta propiedad en el token.
+            ///Tema pagina 2272 pdf core 2.2
+            modelBuilder.Entity<NatilleraEntity>().Property(p => p.RowVersion).IsConcurrencyToken();
+        }
     }
 }
